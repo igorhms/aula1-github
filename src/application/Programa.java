603d2baa239bc8c95d25cbe5d2c7ee558094a634
Igor Machado <igorhms98@gmail.com>
2020-05-08 18:24:15 -0300

    Relatorio gerado

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -11,6 +11,7 @@ public class Programa {
 
 		char sexo;
 		int cerveja, refri, espetinho;
+		double consumo, cover, ingresso, total;
 		
 		System.out.print("Sexo: ");
 		sexo = sc.next().charAt(0);
@@ -22,5 +23,30 @@ public class Programa {
 		espetinho = sc.nextInt();
 		System.out.println();
 
+		if (sexo == 'f' || sexo == 'F'){
+			ingresso = 8.0;
+		}
+		else {
+			ingresso = 10.0;
+		}
+		consumo = cerveja * 5 + refri * 3 + espetinho * 7;
+		if (consumo > 30.0){
+			cover = 0.0;
+		}
+		else {
+			cover = 4.0;
+		}
+		total = ingresso + consumo + cover;
+		
+		System.out.println("RELATÓRIO:");
+		System.out.printf("Consumo = R$ %.2f%n", consumo);
+		if (ingresso == 4.0){
+			System.out.printf("Cover = R$ %.2f%n", cover);
+		}
+		else {
+			System.out.println("Isento de cover");
+		}
+		System.out.printf("Ingresso = R$ %.2f%n%n", ingresso);
+		System.out.printf("Valor a pagar = R$ %.2f%n", total);
 	}
 }
